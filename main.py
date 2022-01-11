c0f73b84fd56a822d14e1c2c7c6325fb1457521b
Varshil-Shah <varshilashah09@gmail.com>
2022-01-11 11:57:51 +0530

    :sparkles: Function to create new file

--- main.py
+++ main.py
@@ -30,7 +30,23 @@ def create_new_project(webapps_folder: str, project_name: str):
     print(colored(f"Project created at ${project_path}", "green"))
 
 
+def create_new_file(webapps_folder: str, project_name: str):
+    src_folder = os.path.join(webapps_folder, project_name, "src")
+
+    filename = input("Enter filepath relative to src folder: ")
+    try:
+        open(os.path.join(src_folder, filename), 'x').close()
+    except FileExistsError:
+        print(colored(f"{filename} already exists", "red"))
+    except FileNotFoundError as e:
+        print(colored(str(e), "red"))
+    else:
+        print(colored(f"{filename} created successfully!", "green"))
+
+
 if __name__ == "__main__":
     tomcat_folder_path = os.getenv("CATALINA_HOME")
     project_name = input("Enter project name: ")
     create_new_project(tomcat_folder_path, project_name)
+    webapps_folder = os.path.join(tomcat_folder_path, "webapps")
+    create_new_file(webapps_folder, project_name)
